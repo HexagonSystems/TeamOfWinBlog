af52c4d5ea1b13bdffda7ce31646dafa1980adf1
Alex Robinson <alex-robinson@live.com>
2013-06-06 14:24:18 +1000

    Templates for LoginView, RegisterView and IndexView

--- view/RegisterViewTemplate.php
+++ view/RegisterViewTemplate.php
@@ -1,9 +1,60 @@
-<header>Register</header>
-<form action="index.php?location=registerPage&action=register" method="POST">
-	<input type="text" placeholder="Username" name="username"><br />
-	<input type="text" placeholder="Email" name="email"><br />
-	<input type="password" placeholder="Password" name="pass"><br />
-	<input type="password" placeholder="Password Again" name="pass2"><br />
-	<input type="submit" value="Register">
-</form>
+<link rel="stylesheet" type="text/css"
+	href="includes/css/registerView.css" />
+<link
+	rel="stylesheet" type="text/css"
+	href="includes/css/index_blog_display.css" />
+<script
+	src="includes/js/register_formValidation.js"></script>
+<script
+	src="includes/js/register_passValidation.js"></script>
 
+<article id="registerBox">
+	<span class="title">Create your account</span>
+	<form action="index.php?location=loginPage&action=register"
+		method="POST">
+		<ul>
+			<!-- Username -->
+			<li><input type="text" placeholder="Username" name="username" size=30
+				id="register_username"
+				onblur="usernameValidation(this.value)">
+				<div id="messageAlert"></div>
+				<div id="message_username" class="messageBox"
+					onclick="hideMessageBox('username')"></div>
+			</li>
+
+			<!-- Email -->
+			<li><input type="text" placeholder="Email" name="email"
+				id="register_email" size=30
+				onblur="formValidation(this.value, 'email')">
+				<div id="message_email" class="messageBox"
+					onclick="hideMessageBox('email')"></div></li>
+
+			<!-- Password -->
+			<li><input type="password" placeholder="Password" name="pass"
+				id="register_pass" size=30 onfocus="openPasswordBox()"
+				onclick="openPasswordBox()" onkeyup="passValidation(this.value)")">
+				<div id="message_pass" class="messageBox"
+					onclick="hideMessageBox('pass')">
+					<Header>Password Strength-O-Metre</Header>
+					<br />
+					<!-- STILL IN PROGRESS -->
+					<!-- DOES NOT UPDATE - FIXED WIDTH -->
+					<div id="passwordStrengthBar">
+						<span></span>
+					</div>
+					<p>Testing</p>
+					<ul>
+						Required:
+						<li>test</li>
+					</ul>
+					<ul>
+						Suggestions
+						<li>test</li>
+					</ul>
+
+				</div></li>
+			<li><input type="submit" value="Sign me up!"></li
+		
+		</ul>
+	</form>
+</article>
