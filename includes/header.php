54b1939a6e36d7668355a29fb68d2f5f2ce541d7
Stephen McMahon <stephentmcm@gmail.com>
2013-06-18 15:25:25 +1000

    UPDATE to latest nav - Missed file changes

--- includes/header.php
+++ includes/header.php
@@ -6,6 +6,12 @@
 <link rel="stylesheet" type="text/css" href="includes/css/main.css" />
 <link rel="stylesheet" type="text/css" href="includes/css/header.css" />
 <link rel="stylesheet" type="text/css" href="includes/css/footer.css" />
+<link rel="stylesheet" type="text/css"	href="includes/css/registerView.css" />
+<link rel="stylesheet" type="text/css" href="includes/css/index_blog_display.css" />
+<script	src="includes/js/register_formValidation.js"></script>
+<script	src="includes/js/register_passValidation.js"></script>
+<link rel="stylesheet" type="text/css" href="includes/css/loginView.css" />
+
 </head>
 <!-- the body section -->
 <body>
